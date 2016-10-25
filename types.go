af762e4a7b2fec0adf65dff1147a7fa2a426f428
kgigitdev <kgigitdev@gmail.com>
2016-10-25 22:41:54 +0300

    Rejigged types.go to allow multiple structs to share fields.

--- types.go
+++ types.go
@@ -5,59 +5,202 @@ import "encoding/json"
 // Season stores information for one or more entries in the
 // "seasons" field of SeriesInformation
 type Season struct {
-	SeasonNumber int  `json:"seasonNumber"`
-	Monitored    bool `json:"monitored"`
+	SeasonNumber int  `json:"seasonNumber,omitempty"`
+	Monitored    bool `json:"monitored,omitempty"`
 }
 
 type SeriesImage struct {
-	CoverType string `json:"coverType"`
-	URL       string `json:"url"`
+	CoverType string `json:"coverType,omitempty"`
+	URL       string `json:"url,omitempty"`
 }
 
 type Ratings struct {
-	Votes int `json:"votes"`
-	Value int `json:"value"`
+	Votes int `json:"votes,omitempty"`
+	Value int `json:"value,omitempty"`
+}
+
+type AddedFragment struct {
+	Added string `json:"added,omitempty"`
+}
+
+type AirTimeFragment struct {
+	AirTime string `json:"airTime,omitempty"`
+}
+
+type CleanTitleFragment struct {
+	CleanTitle string `json:"cleanTitle,omitempty"`
+}
+
+type FirstAiredFragment struct {
+	FirstAired string `json:"firstAired,omitempty"`
+}
+
+type GenresFragment struct {
+	Genres []string `json:"genres,omitempty"`
+}
+
+type ImagesFragment struct {
+	Images []SeriesImage `json:"images,omitempty"`
+}
+
+type IMDBIDFragment struct {
+	IMDBID string `json:"imdbId,omitempty"`
+}
+
+type MonitoredFragment struct {
+	Monitored bool `json:"monitored,omitempty"`
+}
+
+type NetworkFragment struct {
+	Network string `json:"network,omitempty"`
+}
+
+type OverviewFragment struct {
+	Overview string `json:"overview,omitempty"`
+}
+
+type ProfileIDFragment struct {
+	ProfileID int `json:"profileId,omitempty"`
+}
+
+type QualityProfileIDFragment struct {
+	QualityProfileID int `json:"qualityProfileId,omitempty"`
+}
+
+type RatingsFragment struct {
+	Ratings Ratings `json:"ratings,omitempty"`
+}
+
+type RuntimeFragment struct {
+	Runtime int `json:"runtime,omitempty"`
+}
+
+type SeasonCountFragment struct {
+	SeasonCount int `json:"seasonCount,omitempty"`
+}
+
+type SeasonsFragment struct {
+	Seasons []Season `json:"seasons,omitempty"`
+}
+
+type SeriesTypeFragment struct {
+	SeriesType string `json:"seriesType,omitempty"`
+}
+
+type SortTitleFragment struct {
+	SortTitle string `json:"sortTitle,omitempty"`
+}
+
+type StatusFragment struct {
+	Status string `json:"status,omitempty"`
+}
+
+type TagsFragment struct {
+	Tags []string `json:"tags,omitempty"`
+}
+
+type TitleFragment struct {
+	Title string `json:"title,omitempty"`
+}
+
+type TitleSlugFragment struct {
+	TitleSlug string `json:"titleSlug,omitempty"`
+}
+
+type TVMazeIDFragment struct {
+	TVMazeID int `json:"tvMazeId,omitempty"`
+}
+
+type TVRageIDFragment struct {
+	TVRageID int `json:"tvRageId,omitempty"`
+}
+
+type TVDBIDFragment struct {
+	TVDBID int `json:"tvdbId,omitempty"`
+}
+
+type UseSceneNumberingFragment struct {
+	UseSceneNumbering bool `json:"useSceneNumbering,omitempty"`
+}
+
+type YearFragment struct {
+	Year int `json:"year,omitempty"`
+}
+
+type SeasonFolderFragment struct {
+	SeasonFolder bool `json:"seasonFolder,omitempty"`
+}
+
+type IDFragment struct {
+	ID int `json:"id,omitempty"`
+}
+
+// CommonFragments is a compositional struct containing all fragments
+// common to "all series" and "all my series".
+type CommonFragments struct {
+	AddedFragment
+	AirTimeFragment
+	CleanTitleFragment
+	FirstAiredFragment
+	GenresFragment
+	ImagesFragment
+	IMDBIDFragment
+	MonitoredFragment
+	NetworkFragment
+	OverviewFragment
+	ProfileIDFragment
+	QualityProfileIDFragment
+	RatingsFragment
+	RuntimeFragment
+	SeasonCountFragment
+	SeasonFolderFragment
+	SeasonsFragment
+	SeriesTypeFragment
+	StatusFragment
+	TagsFragment
+	TitleFragment
+	TitleSlugFragment
+	TVMazeIDFragment
+	TVDBIDFragment
+	UseSceneNumberingFragment
+	YearFragment
 }
 
 // SeriesInformation is a struct for storing a series information
 type SeriesInformation struct {
-	Title             string        `json:"title"`
-	SortTitle         string        `json:"sortTitle"`
-	SeasonCount       int           `json:"seasonCount"`
-	Status            string        `json:"status"`
-	Overview          string        `json:"overview"`
-	Network           string        `json:"network"`
-	AirTime           string        `json:"airTime"`
-	Images            []SeriesImage `json:"images"`
-	RemotePoster      string        `json:"remotePoster"`
-	Seasons           []Season      `json:"seasons"`
-	Year              int           `json:"year"`
-	ProfileID         int           `json:"profileId"`
-	SeasonFolder      bool          `json:"seasonFolder"`
-	Monitored         bool          `json:"monitored"`
-	UseSceneNumbering bool          `json:"useSceneNumbering"`
-	Runtime           int           `json:"runtime"`
-	TVDBID            int           `json:"tvdbId"`
-	TVRageID          int           `json:"tvRageId"`
-	TVMazeID          int           `json:"tvMazeId"`
-	FirstAired        string        `json:"firstAired"`
-	SeriesType        string        `json:"seriesType"`
-	CleanTitle        string        `json:"cleanTitle"`
-	IMDBID            string        `json:"imdbId"`
-	TitleSlug         string        `json:"titleSlug"`
-	Certification     string        `json:"certification"`
-	Genres            []string      `json:"genres"`
-	Tags              []string      `json:"tags"`
-	Added             string        `json:"added"`
-	Ratings           Ratings       `json:"ratings"`
-	QualityProfileID  int           `json:"qualityProfileId"`
+	CommonFragments
+
+	// Thes might just not have appeared in the JSON output I
+	// happened to see; no guarantee that they can't appear in
+	// "my series"
+	RemotePoster  string `json:"remotePoster,omitempty"`
+	Certification string `json:"certification,omitempty"`
 }
 
 // SeriesInformationList ...
 type SeriesInformationList []SeriesInformation
 
 func (s SeriesInformationList) String() string {
-	j, err := json.MarshalIndent(s, "", "  ")
+	return toJSON(s)
+}
+
+type MySeries struct {
+	IDFragment
+	CommonFragments
+}
+
+type MySeriesList []MySeries
+
+func (m MySeries) String() string {
+	return toJSON(m)
+}
+
+func (m MySeriesList) String() string {
+	return toJSON(m)
+}
+
+func toJSON(i interface{}) string {
+	j, err := json.MarshalIndent(i, "", "  ")
 	if err != nil {
 		return err.Error()
 	}
