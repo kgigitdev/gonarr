e3aee520932fbfef8b8d5197cd21988c2dd819fc
kgigitdev <kgigitdev@gmail.com>
2016-10-25 22:42:58 +0300

    Lots more hacking; trying to get a minimal working version.

--- gonarr.go
+++ gonarr.go
@@ -11,11 +11,12 @@ import (
 
 // Gonarr ...
 type Gonarr struct {
-	Hostname string `json:"hostname"`
-	Port     int    `json:"port"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	APIKey   string `json:"api_key"`
+	Hostname  string `json:"hostname"`
+	Port      int    `json:"port"`
+	ApiPrefix string `json:"api_prefix"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	APIKey    string `json:"api_key"`
 }
 
 func (g Gonarr) String() string {
@@ -45,7 +46,8 @@ func NewGonarrFromConfigFile(path string) *Gonarr {
 }
 
 func (g *Gonarr) makeRequest(command string) []byte {
-	url := fmt.Sprintf("http://%s:%d/api/%s", g.Hostname, g.Port, command)
+	url := fmt.Sprintf("http://%s:%d/%s%s",
+		g.Hostname, g.Port, g.ApiPrefix, command)
 	fmt.Println(url)
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Add("X-Api-Key", g.APIKey)
@@ -73,10 +75,24 @@ func (g *Gonarr) ListCalendar() []byte {
 	return g.makeRequest("calendar")
 }
 
-// ListSeries lists all series in your collection
-func (g *Gonarr) ListSeries() {
-	url := fmt.Sprintf("series")
+// GetAllSeries lists all series in your collection
+func (g *Gonarr) GetAllSeries() MySeriesList {
+	b := g.makeRequest("series")
+	var mySeriesList MySeriesList
+	json.Unmarshal(b, &mySeriesList)
+	return mySeriesList
+}
+
+func (g *Gonarr) GetOneSeries(seriesId int) MySeries {
+	url := fmt.Sprintf("series/%d", seriesId)
 	b := g.makeRequest(url)
+	var mySeries MySeries
+	json.Unmarshal(b, &mySeries)
+	return mySeries
+}
+
+func (g *Gonarr) GetSystemStatus() {
+	b := g.makeRequest("system/status")
 	fmt.Println(string(b))
 }
 
@@ -88,4 +104,35 @@ func (g *Gonarr) SearchSeries(searchTerm string) SeriesInformationList {
 	var seriesInformationList SeriesInformationList
 	json.Unmarshal(b, &seriesInformationList)
 	return seriesInformationList
+}
+
+func (g *Gonarr) PrintJSON(i interface{}) {
+	fmt.Println(toJSON(i))
+}
+
+func (g *Gonarr) Filter(i interface{}, keys ...string) interface{} {
+	mkeys := make(map[string]bool)
+	for _, k := range keys {
+		mkeys[k] = true
+	}
+
+	sl, ok := i.([]interface{})
+	if ok {
+		var o []interface{}
+		for _, e := range sl {
+			o = append(o, g.Filter(e, keys...))
+		}
+		return o
+	}
+	m, ok := i.(map[string]interface{})
+	if ok {
+		o := make(map[string]interface{})
+		for k, v := range m {
+			if _, haskey := mkeys[k]; haskey {
+				o[k] = g.Filter(v, keys...)
+			}
+		}
+		return o
+	}
+	return i
 }
--- gonarr/main.go
+++ gonarr/main.go
@@ -13,6 +13,13 @@ var opts struct {
 	Config     string `short:"c" long:"config" description:"config file" default:"gonarr.json"`
 	Search     string `short:"s" long:"search" description:"Search series"`
 	ListSeries bool   `short:"l" long:"list-series" description:"List all series in your collection"`
+	Info       int    `short:"i" long:"info" description:"Show info about one series in your collection"`
+	Season     int    `long:"season" description:"Season (for modifying stuff)"`
+
+	ToggleMonitor bool `short:"m" long:"toggle-monitor" description:"Season (for modifying stuff)"`
+	Status        bool `long:"status" description:"Get system status"`
+
+	Full bool `long:"full" description:"List full JSON"`
 }
 
 func main() {
@@ -30,8 +37,17 @@ func main() {
 
 	log.Println("Gonarr is: ", g)
 
-	if opts.ListSeries {
-		g.ListSeries()
+	if opts.Status {
+		g.GetSystemStatus()
+	} else if opts.ListSeries {
+		series := g.GetAllSeries()
+		fmt.Println(series)
+	} else if opts.Info > 0 {
+		series := g.GetOneSeries(opts.Info)
+		fmt.Println(series)
+		if opts.ToggleMonitor && opts.Season > 1 {
+			log.Println("Toggling montoring flag ...")
+		}
 	} else if opts.Search != "" {
 		fmt.Println(g.SearchSeries(opts.Search))
 	}
